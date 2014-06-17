2e99d961154bfe26dfac25f5f640bf6f2a9dd562
https://github.com/settings/profile <shashank.agrawal@onmobile.com>
2014-06-17 20:43:44 +1000

    With report generate

--- src/com/mobile/tool/stock/manager/listener/AdminRoleMouseListener.java
+++ src/com/mobile/tool/stock/manager/listener/AdminRoleMouseListener.java
@@ -1,14 +1,25 @@
 package com.mobile.tool.stock.manager.listener;
 
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.sql.Date;
+import java.util.List;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JTree;
 import javax.swing.tree.TreePath;
 
+import net.sf.dynamicreports.jasper.builder.JasperReportBuilder;
+import net.sf.dynamicreports.report.exception.DRException;
+import net.sourceforge.jdatepicker.impl.JDatePanelImpl;
+import net.sourceforge.jdatepicker.impl.JDatePickerImpl;
+import net.sourceforge.jdatepicker.impl.SqlDateModel;
+import net.sourceforge.jdatepicker.impl.UtilDateModel;
+
+import com.mobile.tool.stock.manager.entity.SalesRecord;
 import com.mobile.tool.stock.manager.entity.UserLoginDetails;
 import com.mobile.tool.stock.manager.handler.CustomerDetailsOptionHandler;
 import com.mobile.tool.stock.manager.handler.EmployeeDetailsOptionHandler;
@@ -16,7 +27,10 @@ import com.mobile.tool.stock.manager.handler.ProductDetailsOptionHandler;
 import com.mobile.tool.stock.manager.handler.RecordKeepingOptionHandler;
 import com.mobile.tool.stock.manager.handler.SalesHistoryOptionHandler;
 import com.mobile.tool.stock.manager.handler.VendorDetailsOptionHandler;
+import com.mobile.tool.stock.manager.model.SalesReportData;
 import com.mobile.tool.stock.manager.model.StockManagementTableModel;
+import com.mobile.tool.stock.manager.report.DynamicReportDesign;
+import com.mobile.tool.stock.manager.repository.SaleRecordsRepository;
 
 public class AdminRoleMouseListener  extends MouseAdapter {
 
@@ -26,6 +40,9 @@ public class AdminRoleMouseListener  extends MouseAdapter {
 	private String lastSelection;
 	private JPanel btnPnl;
 	private JButton button;
+	private JDatePickerImpl fromDatePicker;
+	private JDatePickerImpl toDatePicker;
+	
 	
 	public AdminRoleMouseListener() {
 	}
@@ -98,6 +115,45 @@ public class AdminRoleMouseListener  extends MouseAdapter {
 				addButtonsInPanel("Add Product", new AddCustomerRecordListener(this));
 				this.lastSelection = "Product Order";
 			} 
+		}else if("Sales Reports".equalsIgnoreCase(tp.getLastPathComponent()
+				.toString())){
+			if(!"Sales Reports".equalsIgnoreCase(lastSelection)) {
+				tableModel.refreshTableData(new String[][]{}, new String[]{});
+				if (btnPnl != null) {
+					if(button!=null)
+						btnPnl.remove(button);
+					SqlDateModel fromDateModel = new SqlDateModel();
+					JDatePanelImpl fromDatePanel = new JDatePanelImpl(fromDateModel);
+					fromDatePicker = new JDatePickerImpl(fromDatePanel);
+					btnPnl.add(fromDatePicker);
+					
+					SqlDateModel toDateModel = new SqlDateModel();
+					JDatePanelImpl toDatePanel = new JDatePanelImpl(toDateModel);
+					toDatePicker = new JDatePickerImpl(toDatePanel);
+					btnPnl.add(toDatePicker);
+					
+					button = (new JButton("Get Report"));
+					button.addActionListener(new ActionListener() {
+						
+						@Override
+						public void actionPerformed(ActionEvent e) {
+							try {
+								Date fromDate = (Date) fromDatePicker.getModel().getValue();
+								Date toDate = (Date) toDatePicker.getModel().getValue();
+								List<SalesRecord> salesRecords = SaleRecordsRepository.getSaleRecordsForDatePeriod(fromDate, toDate);
+								DynamicReportDesign design = new DynamicReportDesign(new SalesReportData(salesRecords));
+								JasperReportBuilder report = design.build();
+								report.show();
+							} catch (DRException e1) {
+								e1.printStackTrace();
+							}
+						}
+					});
+					btnPnl.add(button);
+					btnPnl.revalidate();
+				}
+				this.lastSelection = "Sales Reports";
+			} 
 		}
 
 	}
@@ -111,6 +167,11 @@ public class AdminRoleMouseListener  extends MouseAdapter {
 		if (btnPnl != null) {
 			if(button!=null)
 				btnPnl.remove(button);
+			if(fromDatePicker!=null || toDatePicker!=null){
+				btnPnl.remove(fromDatePicker);
+				btnPnl.remove(toDatePicker);
+			}
+				
 			button = (new JButton(buttonName));
 			button.addActionListener(actionListener);
 			btnPnl.add(button);
@@ -121,4 +182,5 @@ public class AdminRoleMouseListener  extends MouseAdapter {
 	public void reloadRecordKeepingTableData() {
 		RecordKeepingOptionHandler.handleRecordKeeping(tableModel);
 	}
+	
 }
